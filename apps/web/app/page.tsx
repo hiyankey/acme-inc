10098130f24e16849d383dd1df37a05d271f58e9
Emmanuel Yankey <hiyankey@gmail.com>
2025-02-19 18:52:41 +0000

    wip

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,11 @@ export default async function Home() {
   const user = await prisma.user.findFirst()
   return (
                   <div className="font-sans">
-      {user?.name ?? "No user added yet"}
+      {user?.name ?? <p className="mr-4">No user added yet</p>} 
+    <div className="relative w-3 h-3 inline-flex justify-center">
+      <span className="absolute bg-red-400 opacity-75 rounded-full w-full h-full inline-flex animate-ping " />
+      <span className="relative bg-red-600 rounded-full w-3 h-3 inline-flex"/>
+    </div>
     </div>
   );
 }
